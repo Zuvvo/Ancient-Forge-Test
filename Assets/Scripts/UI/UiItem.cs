1129860f35f37e4d9b3d5721a0fabb06cdb1136e
Zuvvo <Zuvvo4@gmail.com>
2025-03-21 21:10:56 +0100

    base ui layout, items added

---
+++ Assets/Scripts/UI/UiItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiItem : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _amountText;
+    [SerializeField] private Image _image;
+
+    public void Setup(ItemData data)
+    {
+
+    }
+}
